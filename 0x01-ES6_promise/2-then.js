38f4c7476fe604819871868f9c2cd16e4199e407
horpeazy <iyamuhope.nosa647@gmail.com>
2023-06-28 00:34:10 +0100

    feat: ES6 Promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    const response = {
+      status: 200,
+      body: 'success',
+    };
+    return response;
+  })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
