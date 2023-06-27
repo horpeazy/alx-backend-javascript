1b34560947d0febeaa0aaf1ec513edb4a96e9e44
horpeazy <iyamuhope.nosa647@gmail.com>
2023-06-27 03:18:30 +0100

    feat: es6 basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arrayItems = array.keys();
-  for (const idx of arrayItems) {
+  for (const idx of array.keys()) {
     const value = array[idx];
     // eslint-disable-next-line no-param-reassign
     array[idx] = appendString + value;
