4015b9650f2e4a46f22c81ad5bfc7f9969356bde
horpeazy <iyamuhope.nosa647@gmail.com>
2023-08-23 02:18:01 +0100

    feat: nodejs basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const chunck = process.stdin.read();
-  if (chunck !== null) {
+  if (chunck) {
     process.stdout.write(`Your name is: ${chunck}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
