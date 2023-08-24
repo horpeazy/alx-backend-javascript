bb800b14729fa51f2813ac6c2f6b34d1e66dc5d8
horpeazy <iyamuhope.nosa647@gmail.com>
2023-08-24 12:31:11 +0100

    feat: uniitests

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,13 @@
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const Utils = require("./utils.js");
+const sendPaymentRequestToApi = require("./3-payment");
+
+
+describe("sendPaymentRequestToApi", function() {
+  it("validate usage of Utils", function() {
+    calculateNumberSpy = sinon.spy(Utils, "calculateNumber");
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+  })
+})
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,21 @@
+Utils = {
+  calculateNumber: function(type, a, b) {
+    roundedA = Math.round(a);
+    roundedB = Math.round(b);
+
+    if (type === 'SUM') {
+      return roundedA + roundedB;
+    } else if (type === 'SUBTRACT') {
+      return roundedA - roundedB;
+    } else if (type === 'DIVIDE') {
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return (roundedA / roundedB) ;
+    } else {
+      return 'Error';
+    }
+  }
+}
+
+module.exports = Utils
